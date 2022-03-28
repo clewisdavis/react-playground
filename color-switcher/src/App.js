042267e9308cc24babcaddc4de43dab4dff7e400
Chris Davis <chrislewis.davis@gmail.com>
2022-03-28 08:26:40 -0400

    experiments

--- color-switcher/src/App.js
+++ color-switcher/src/App.js
@@ -4,14 +4,15 @@ import './App.css';
 import ColorChangeButton from './ColorChangeButton'
 
 function App() {
-  const [color, setColor] = useState('')
+  const [color, setColor] = useState('');
+  const [active, setActive] = useState('');
   return (
     <div className={`react-root ${color}`}>
       <div className='centered'>
         <h1>Color Picker</h1>
-        <ColorChangeButton color='red' type='button' setColor={setColor} />
-        <ColorChangeButton color='blue' type='button' setColor={setColor} />
-        <ColorChangeButton color='yellow' type='button' setColor={setColor} />
+        <ColorChangeButton active='active' setActive={setActive} color='red' type='button' setColor={setColor} />
+        <ColorChangeButton active='active' setActive={setActive} color='blue' type='button' setColor={setColor} />
+        <ColorChangeButton active='active' setActive={setActive} color='yellow' type='button' setColor={setColor} />
       </div>
     </div>
   );
--- color-switcher/src/ColorChangeButton.js
+++ color-switcher/src/ColorChangeButton.js
@@ -4,7 +4,15 @@ import React from 'react'
 function ColorChangeButton (props) {
     console.log(props);
     return (
-        <button type={props.type} className={props.color} onClick={() => props.setColor(props.color)}>
+        <button 
+            type={props.type} 
+            className={props.color}
+            onClick={function() {
+                props.setColor(props.color);
+                console.log('function here and a ' + props.active);
+                props.setActive(props.active);
+            }
+            }>
             {props.color}
         </button>
     )
